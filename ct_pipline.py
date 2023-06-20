8a188c668b27006ef2b519a224f72aaa68a6ab97
ctran722 <136828285+ctran722@users.noreply.github.com>
2023-06-20 15:02:24 -0400

    Add files via upload

---
+++ ct_pipline.py
@@ -0,0 +1,14 @@
+#create array
+Color_Array= ["Red","Blue","Green"]
+
+# create function
+def call()
+    
+
+# r=Color_Array[0]
+# Def
+
+# for loop to go through array
+for t in Color_Array:
+    print(t)
+
